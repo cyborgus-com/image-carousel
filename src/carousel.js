612f636d0856a27190ab453e2d90bf8c50bf49bd
karolisurbonas <karolis.urbonas@gmail.com>
2025-12-15 20:39:56 -0600

    buttons workl but dont move as they should, debug tmrw

--- src/carousel.js
+++ src/carousel.js
@@ -5,10 +5,31 @@ const carousel = document.querySelector(".carousel");
 const step = container.clientWidth;
 // const currPos = document.querySelector(".currentpos");
 
-fwdButton.addEventListener("click", (e) => {
-  carousel.style.transform = "translateX(-500px)";
+function getX() {
+  const style = window.getComputedStyle(carousel);
+  const matrix = style.transform;
+  if (matrix === "none") return 0;
+
+  const values = matrix.split("(")[1].split(")")[0].split(",");
+  //   return parseFloat(values[4]) || 0;
+  if (matrix.startsWith("matrix3d")) {
+    return parseFloat(values[12]) || 0;
+  } else return parseFloat(values[4]) || 0;
+}
+
+function setX(x) {
+  const minX = -(carousel.clientWidth - container.clientWidth);
+  const maxX = 0;
+  x = Math.max(minX, Math.min(maxX, x));
+  carousel.style.transform = `translateX(${x}px)`;
+}
+
+fwdButton.addEventListener("click", () => {
+  //   carousel.style.transform = "translateX(-500px)";
+  setX(getX() - step);
 });
 
-backButton.addEventListener("click", (e) => {
-  carousel.style.transform = "translateX(0px)";
+backButton.addEventListener("click", () => {
+  //   carousel.style.transform = "translateX(0px)";
+  setX(getX() + step);
 });
